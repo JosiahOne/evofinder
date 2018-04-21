2d3a930c2f92ce259d837d6d6beaf33db33d8511
ctyh <callumhutchinson@googlemail.com>
2018-04-21 14:15:42 -0400

    changed error handling

--- run.sh
+++ run.sh
@@ -2,12 +2,10 @@ function setup_err(){
   echo "Need to run setup.sh"
   exit 1
 }
+# calls setup_err() if the virtual env can't be activated
 source env/bin/activate || setup_err
-if [ "$#" -lt "2" ] ; then
-  echo "Usage: ./run.sh python3 evofinder.py <input python file> <target_line_num> <example_file_1> <example_file_2> ..."
-  exit 1
-elif [ ! $1  = "python3" ] || [ ! $2 = "evofinder.py" ] ; then 
-  echo "Usage: ./run.sh python3 evofinder.py <input python file> <target_line_num> <example_file_1> <example_file_2> ..."
+if [ "$#" -lt "1" ] ; then
+  echo "Usage: ./run.sh <shell_commands>"
   exit 1
 fi
 $arg
